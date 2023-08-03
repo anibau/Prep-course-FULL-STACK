bb274efbf6b806f09f209896629fc41f8fff1750
anibau <miriambt678@gmail.com>
2023-08-02 22:31:59 -0500

    prueba1

---
+++ operacionesDeComparacion.js
@@ -0,0 +1,21 @@
+console.log (4<7);
+console.log(4<1);
+console.log(4>4)
+console.log(4==7)
+
+console.log (3 == 3) ;
+console.log (3 === 3) ;
+console.log (3 == '3') ;
+console.log (3 === '3') ;
+console.log (7 == '7') ;
+console.log (7 === '7')
+
+var a= 1;
+var b= 5;
+var c=a=b;
+console.log(a) //5
+console.log (b)
+console.log(c) 
+
+console.log(30/3/2)
+console.log(100/2/5)
