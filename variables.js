daa64e2b510fc7e8845080f0c9ceef4849b5f899
anibau <miriambt678@gmail.com>
2023-08-02 20:40:42 -0500

    pruebavariables

---
+++ variables.js
@@ -0,0 +1,13 @@
+// STRING
+var bootcamp = 'henry';
+// numeros
+var numeroEntero = 14
+// booleanos
+var estoyAburrido = false
+// undedined
+var cajon1; 
+console.log (cajon1)
+// null
+var sinnada = null
+
+
